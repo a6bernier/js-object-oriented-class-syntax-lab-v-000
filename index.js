f1df2dc9ca7735c1acb16012fc0ead4fee2ae6ee
Andrew Bernier <a6bernier@gmail.com>
2018-12-19 00:59:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class BoardMember {
     return "You will see great things from Scuber."
   }
 
-  BoardMember.prototype.sayHi = function(){
+  sayHi() = {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
