d9c44b14ca17d9b3130cafbe96e195390aa884c8
Andrew Bernier <a6bernier@gmail.com>
2018-12-19 01:03:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,13 +24,7 @@ class BoardMember {
   }
 }
 
-class Ceo {
-  constructor(name, homeState, training){
-    this.name = name
-    this.homeState = homeState
-    this.training = training
-  }
-
+class Ceo extends BoardMember {
   hireEmployee() {
     return `Welcome aboard!`
   }
